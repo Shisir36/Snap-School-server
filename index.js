2656788b2b221fe773d2b120485cc025d965b241
Al Mamun <almamunshisir770@gmail.com>
2023-06-11 01:02:25 +0600

    jwt token impliment

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware 
 app.use(cors());
@@ -37,6 +38,13 @@ async function run() {
         const classesCollections = client.db("snap-school").collection('classes');
         const instructorsCollections = client.db("snap-school").collection('instructors');
         const classCartCollections = client.db("snap-school").collection('classCart');
+        const usersCollection = client.db("snap-school").collection("users");
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
 
         app.get('/classes', async (req, res) => {
             const cursor = classesCollections.find();
@@ -48,6 +56,39 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+        // users
+        // verifyJWT, verifyAdmin,
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        });
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            console.log(existingUser);
+
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+
+        })
         app.get('/classesCart', async (req, res) => {
             const email = req.query.email;
 
@@ -62,6 +103,50 @@ async function run() {
             const addedClass = req.body
             const result = await classCartCollections.insertOne(addedClass)
             res.send(result)
+        })
+        app.post('/classes', async (req, res) => {
+            const newClass = req.body
+            const result = await classesCollections.insertOne(newClass);
+            res.send(result);
+        });
+        app.get('/classes', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = { email: req.query.email };
+            }
+            const result = await classesCollections.find(query).toArray();
+            res.send(result);
+        });
+        app.patch('/classes/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+
+            let updateDoc;
+
+            if (req.body.action === 'approve') {
+                updateDoc = {
+                    $set: {
+                        status: 'approved'
+                    }
+                };
+            } else if (req.body.action === 'deny') {
+                updateDoc = {
+                    $set: {
+                        status: 'denied'
+                    }
+                };
+            } else if (req.body.action === 'feedback') {
+                updateDoc = {
+                    $set: {
+                        feedback: req.body.feedback
+                    }
+                };
+            } else {
+                return res.status(400).json({ error: 'Invalid action' });
+            }
+
+            const result = await classesCollections.findOneAndUpdate(filter, updateDoc);
+            res.send(result);
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
